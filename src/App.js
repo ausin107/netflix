d901b607fa7a0d7644893e893dfa6bc35172d5a0
ausin107 <78475645+ausin107@users.noreply.github.com>
2022-02-22 10:42:35 +0700

    add pages, component, library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import {Routes, Route, Link} from 'react-router-dom'
+import {useNavigate} from 'react-router'
+import Home from './Pages/Home';
 function App() {
+  const navigate = useNavigate()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='ml-4 text-red-500'>
+      <nav className=''>
+      <Link to='/browse'>Trang chủ</Link>
+      </nav>
+      <Routes>
+        <Route path='/browse' element={<Home />} />
+      </Routes>
     </div>
   );
 }
